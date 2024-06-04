6f96668194c2f6cb00e38819607283a51e42433b
Flavio <bsilvaflav@gmail.com>
2024-06-03 21:55:18 -0300

    velas

---
+++ e-commerce/scripts/produtos.js
@@ -0,0 +1,73 @@
+let produtos = [
+    {
+        img: "imagens/BanhoDeMarVelaAromatica300g39.png",
+        dsImg: "vela de mar",
+        nome: "Vela de Mar",
+        fatias6: 35.00,
+        fatias8: 45.00,
+        fatias12: 55.00
+    },
+    {
+        img: "imagens/CampoDeLavandasVelaAromatica300g39.png",
+        dsImg: "campo de lavanda",
+        nome: "Campo de Lavanda",
+        fatias6: 35.00,
+        fatias8: 45.00,
+        fatias12: 55.00
+    },
+    {
+        img: "imagens/FloresVivasVelaAromatica300g39.png",
+        dsImg: "flores vivas",
+        nome: "Flores Vivas",
+        fatias6: 35.00,
+        fatias8: 45.00,
+        fatias12: 55.00
+    },
+    {
+        img: "imagens/MeuMomentoVelaAromatica300g39.png",
+        dsImg: "meu momento",
+        nome: "Meu Momento",
+        fatias6: 35.00,
+        fatias8: 45.00,
+        fatias12: 55.00
+    },
+    {
+        img: "imagens/MeusCristaisVelaAromatica300g39.png",
+        dsImg: "meus cristais",
+        nome: "Meus Cristais",
+        fatias6: 35.00,
+        fatias8: 45.00,
+        fatias12: 55.00
+    },
+    {
+        img: "imagens/MinhaRotinaVelaAromatica300g39.png",
+        dsImg: "minha rotina",
+        nome: "Minha Rotina",
+        fatias6: 35.00,
+        fatias8: 45.00,
+        fatias12: 55.00
+    },
+    {
+        img: "imagens/PasseioNaFlorestaVelaAromatica300g39.png",
+        dsImg: "passeio na floresta",
+        nome: "Passeio na Floresta",
+        fatias6: 35.00,
+        fatias8: 45.00,
+        fatias12: 55.00
+    },
+    {
+        img: "imagens/TrioBomDiaVelaAromatica300g39.png",
+        dsImg: "trio bom dia",
+        nome: "Trio Bom Dia",
+        fatias6: 35.00,
+        fatias8: 45.00,
+        fatias12: 55.00
+    },
+    {
+        img: "imagens/VidaNoCampoVelaAromatica300g39.png",
+        dsImg: "vida no campo",
+        nome: "Vida no Campo",
+        fatias6: 35.00,
+        fatias8: 45.00,
+        fatias12: 55.00
+    }]
---
+++ e-commerce/scripts/script.js
@@ -0,0 +1,107 @@
+let menuContent = document.querySelector('.content');
+let menuToggle = menuContent.querySelector('.menu-toggle');
+let show = true;
+
+menuToggle.addEventListener('click', () => {
+    document.body.style.overflow = show ? 'hidden' : 'initial';
+    menuContent.classList.toggle('on', show);
+    show = !show;
+})
+
+let itensVela = document.querySelector(".itens-vela");
+let id = 0;
+for (const p of produtos) {
+    itensVela.innerHTML += `
+    <div class="">
+        <img src="${p.img}" alt="${p.dsImg}">
+        <div class="info">
+            <h3>${p.nome}</h3>
+            <h4>6 fatias<span>R$${p.fatias6}</span></h4>
+            <h4>8 fatias<span>R$${p.fatias8}</span></h4>
+            <h4>12 fatias<span>R$${p.fatias12}</span></h4>
+            <button id="id${id}" class="pedir">pedir agora</button>
+        </div>
+    </div>
+    `;
+    id++;
+}
+
+let telaCarrinho = document.querySelector('.tela-carrinho');
+let continuar = document.querySelector('.continuar');
+continuar.addEventListener('click', ()=>{
+    telaCarrinho.classList.toggle('ocultar-tela-carrinho');
+})
+
+let compras = document.querySelector('.compras');
+compras.addEventListener('click', ()=>{
+    telaCarrinho.classList.toggle('ocultar-tela-carrinho');
+    menuToggle.click(); // fechar o menu suspenso nas versões mobile
+})
+
+let lsPedido = document.querySelectorAll('.pedir');
+for (const bt of lsPedido) {
+    bt.addEventListener('click', ()=>{
+        let id = bt.id.replace('id','');
+        produtos[id].quantidade = 1;
+        atualizarTabela();
+    });
+}
+
+let tbody = document.querySelector('tbody');
+function atualizarTabela() {
+    tbody.innerHTML = '';
+    let total = 0;
+    let id = 0;
+    for (const p of produtos) {
+        if (p.quantidade > 0) {
+            tbody.innerHTML += `
+            <tr>
+                <td>${p.nome}</td>
+                <td>${p.quantidade}x${p.fatias8}=${p.quantidade*p.fatias8}(8 fatias)</td>
+                <td>
+                    <i class="bi bi-plus-square-fill" id="plus${id}"></i>
+                    <i class="bi bi-dash-square-fill" id="dash${id}"></i>
+                </td>
+            </tr>`;
+            total += p.quantidade*p.fatias8;
+        }
+        id++;
+    }
+    document.querySelector('#total-pedido').innerHTML = `Valor total do pedido = R$${total}`;
+    atualizarPlusDash('plus');
+    atualizarPlusDash('dash');
+}
+
+function atualizarPlusDash(tipo){
+    let botoes = document.querySelectorAll(`.bi-${tipo}-square-fill`);
+    for (const bt of botoes){
+        bt.addEventListener('click',()=>{
+            let id = bt.id.replace(tipo,'');
+            if(tipo == 'plus'){
+                produtos[id].quantidade++;
+            }
+            if(tipo == 'dash'){
+                produtos[id].quantidade--;
+            }
+            atualizarTabela();
+        });
+        
+    }
+}
+
+let enviar = document.querySelector('.enviar');
+enviar.addEventListener('click', ()=>{
+    let msg ='Gostaria de fazer o seguinte pedido\n';
+    let total = 0;
+    for (const p of produtos) {
+        if (p.quantidade > 0) {
+            msg+= `${p.nome} ${p.quantidade}x${p.fatias8}=${p.quantidade*p.fatias8}\n`;
+            total += p.quantidade*p.fatias8;
+        }
+    }
+    msg +=`Total = ${total}`;
+    msg = encodeURI(msg);
+    let fone ='556198593743';
+    let link =`https://api.whatsapp.com/send?phone=${fone}&text=${msg}`;
+    window.open(link);
+});
