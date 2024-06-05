87ee726061ae8a5b10453af41b3c54819101775a
Flavio <bsilvaflav@gmail.com>
2024-06-04 21:56:58 -0300

    ajuste tela carrinho

--- e-commerce/scripts/script.js
+++ e-commerce/scripts/script.js
@@ -16,9 +16,9 @@ for (const p of produtos) {
         <img src="${p.img}" alt="${p.dsImg}">
         <div class="info">
             <h3>${p.nome}</h3>
-            <h4>6 fatias<span>R$${p.fatias6}</span></h4>
-            <h4>8 fatias<span>R$${p.fatias8}</span></h4>
-            <h4>12 fatias<span>R$${p.fatias12}</span></h4>
+            <h4>Pequena <span>R$${p.fatias6}</span></h4>
+            <h4>Media <span>R$${p.fatias8}</span></h4>
+            <h4>Grande <span>R$${p.fatias12}</span></h4>
             <button id="id${id}" class="pedir">pedir agora</button>
         </div>
     </div>
